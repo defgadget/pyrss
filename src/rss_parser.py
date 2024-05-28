4e7ecccd9e94ebbda56a8173b2e365100f77e967
DefGadget <tgauby@gmail.com>
2024-05-27 20:31:38 -0500

    (feat) provide custom filtering for rss tags

--- src/rss_parser.py
+++ src/rss_parser.py
@@ -2,30 +2,43 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 
 
-def parse_rss_feed(xml: str) -> dict:
-    root = ET.fromstring(xml)
+class RSSParser:
+    def __init__(self, includes: list[str] = []):
+        if len(includes) == 0 or includes is None:
+            self.includes = ["title", "link", "description", "pubDate"]
+        else:
+            self.includes = includes
 
-    include = ["title", "link", "description", "pubDate"]
-    rss_info = {}
-    articles = []
+    def parse_rss_feed(self, xml: str) -> dict:
+        root = ET.fromstring(xml)
 
-    for channel in root.findall("channel"):
-        title = channel.find("title")
-        if title is not None:
-            rss_info["publisher"] = title.text
+        rss_info = {}
+        articles = []
 
-        for item in channel.findall("item"):
-            article = {}
-            for each in item.findall("./*"):
-                if each.tag in include:
-                    value = each.text
-                    if each.tag == "pubDate" and value is not None:
-                        formatted_date = datetime.strptime(
-                            value, "%a, %d %b %Y %I:%M:%S %Z"
-                        ).strftime("%Y-%m-%d")
-                        value = formatted_date
-                    article[each.tag] = value
-            articles.append(article)
+        for channel in root.findall("channel"):
+            title = channel.find("title")
+            if title is not None:
+                rss_info["publisher"] = title.text
 
-    rss_info["articles"] = articles
-    return rss_info
+            for item in channel.findall("item"):
+                article = {}
+                for each in item.findall("./*"):
+                    if each.tag in self.includes:
+                        value = each.text
+                        if each.tag == "pubDate" and value is not None:
+                            formatted_date = datetime.strptime(
+                                value, "%a, %d %b %Y %I:%M:%S %Z"
+                            ).strftime("%Y-%m-%d")
+                            value = formatted_date
+                        article[each.tag] = value
+                articles.append(article)
+
+        rss_info["articles"] = articles
+        return rss_info
+
+    def parse_multi_rss_feeds(self, xmls: list[str]) -> list[dict]:
+        feeds = []
+        for feed in xmls:
+            parsed = self.parse_rss_feed(feed)
+            feeds.append(parsed)
+        return feeds
--- src/test_rss_parser.py
+++ src/test_rss_parser.py
@@ -1,6 +1,6 @@
 import unittest
 
-import rss_parser
+from rss_parser import RSSParser
 
 
 class TestRSSParser(unittest.TestCase):
@@ -19,7 +19,8 @@ class TestRSSParser(unittest.TestCase):
     </channel>
 </rss>
 """
-        parsed = rss_parser.parse_rss_feed(xml)
+        parser = RSSParser()
+        parsed = parser.parse_rss_feed(xml)
         expect = {
             "publisher": "Test Title",
             "articles": [
@@ -28,6 +29,91 @@ class TestRSSParser(unittest.TestCase):
                     "link": "https://test.com/test",
                     "description": "This is a test description",
                     "pubDate": "2024-01-01",
+                }
+            ],
+        }
+        self.assertDictEqual(parsed, expect)
+
+    def test_parse_multi_rss_feeds(self):
+        xml = """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Test Title</title>
+        <link>https://test.com</link>
+        <item>
+            <title>Test Article</title>
+            <link>https://test.com/test</link>
+            <description>This is a test description</description>
+            <pubDate>Tue, 01 Jan 2024 11:06:42 GMT</pubDate>
+        </item>
+    </channel>
+</rss>
+"""
+        xml2 = """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Another Test Title</title>
+        <link>https://anothertest.com</link>
+        <item>
+            <title>Another Test Article</title>
+            <link>https://anothertest.com/test</link>
+            <description>This is a test description</description>
+            <pubDate>Tue, 01 Jun 2024 11:06:42 GMT</pubDate>
+        </item>
+    </channel>
+</rss>
+"""
+        parser = RSSParser()
+        parsed = parser.parse_multi_rss_feeds([xml, xml2])
+        expect = [
+            {
+                "publisher": "Test Title",
+                "articles": [
+                    {
+                        "title": "Test Article",
+                        "link": "https://test.com/test",
+                        "description": "This is a test description",
+                        "pubDate": "2024-01-01",
+                    }
+                ],
+            },
+            {
+                "publisher": "Another Test Title",
+                "articles": [
+                    {
+                        "title": "Another Test Article",
+                        "link": "https://anothertest.com/test",
+                        "description": "This is a test description",
+                        "pubDate": "2024-06-01",
+                    }
+                ],
+            },
+        ]
+        self.assertListEqual(parsed, expect)
+
+    def test_parse_rss_feed_with_custom_includes(self):
+        xml = """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Test Title</title>
+        <link>https://test.com</link>
+        <item>
+            <title>Test Article</title>
+            <link>https://test.com/test</link>
+            <description>This is a test description</description>
+            <pubDate>Tue, 01 Jan 2024 11:06:42 GMT</pubDate>
+        </item>
+    </channel>
+</rss>
+"""
+        parser = RSSParser(includes=["title", "link"])
+        parsed = parser.parse_rss_feed(xml)
+        expect = {
+            "publisher": "Test Title",
+            "articles": [
+                {
+                    "title": "Test Article",
+                    "link": "https://test.com/test",
                 }
             ],
         }
